4d61f02f31d94709b7232f31ca9b77b809216fe4
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2026-03-05 18:36:50 -0500

    Create method to generate random code
    Inside the initialize method, an array containing all available colors
    is created. This array will be used as the pool from which random colors
    are selected. An empty array called @code is also initialized; this array
    will store the colors that make up the generated code.
    
    The create_code method fills the @code array with four randomly selected
    colors from the colors array. It uses a loop that runs four times, selecting
    one random color during each iteration and adding it to @code.
    
    After the loop finishes, the method returns the generated code.

--- lib/codemaker.rb
+++ lib/codemaker.rb
@@ -1,4 +1,17 @@
 
 class CodeMaker 
   
+  def initialize()
+    @colors = ['purple','red','white','black','yellow','blue','green','brown','pink','orange']
+    @code = []
+  end
+
+  def create_code
+    4.times do
+      @code.push(@colors[rand(0..10)])
+    end
+
+    @code
+  end
+
 end
