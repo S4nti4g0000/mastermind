8429a8d7b4bdb7a3cc49300890c8b2c028307aeb
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2026-03-25 19:00:39 -0500

    Create method to read and store player's choice
    The player will be asked four times to enter a color, then it will return
    the resulting array.

--- lib/player.rb
+++ lib/player.rb
@@ -1,4 +1,21 @@
 
 class Player 
   
+  attr_reader :choice
+
+  def initialize
+    @choice = []    
+  end
+
+  def player_choice
+    
+    4.times do 
+      print "Enter a color: "
+      @choice << gets.chomp
+    end
+
+    @choice
+
+  end
+
 end
