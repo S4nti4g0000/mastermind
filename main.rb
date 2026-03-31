dabd2858e85c806a66cc576a60efbfa01f485f73
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2026-03-31 18:00:26 -0500

    Create initial game loop

--- main.rb
+++ main.rb
@@ -4,3 +4,21 @@ require_relative 'lib/comparison'
 
 codemaker = CodeMaker.new
 player = Player.new
+
+codemaker.create_code
+
+round_count = 0
+
+while round_count < 5
+  player.choice.clear if !player.choice.empty?
+
+  puts "\n -- Round ##{round_count+1} -- \n"
+
+  player.player_choice
+  player.choice
+
+  Comparison.compare_choices(player.choice, codemaker.code)
+
+  round_count += 1
+
+end
