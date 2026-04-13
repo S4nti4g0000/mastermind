3efcfb9a9e26d3b1b42512fe281701550678dcb4
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2026-04-13 17:41:40 -0500

    Finish game loop
    Added an extra condition to terminate the loop and implemented a comparison
    to check whether the player successfully guessed the code. Also introduced
    a check when the round limit is reached to prompt the player to decide if
    they want to play again.

--- main.rb
+++ main.rb
@@ -8,8 +8,14 @@ player = Player.new
 codemaker.create_code
 
 round_count = 0
+game_on = true
+choice = ""
 
-while round_count < 5
+codemaker.code
+
+while round_count <= 5 && game_on
+
+  
   player.choice.clear if !player.choice.empty?
 
   puts "\n -- Round ##{round_count+1} -- \n"
@@ -19,6 +25,27 @@ while round_count < 5
 
   Comparison.compare_choices(player.choice, codemaker.code)
 
+  if Comparison.winner(player.choice, codemaker.code)
+    puts "You win!!!"
+    game_on = false
+  end
+  
   round_count += 1
+
+  if round_count > 5
+    puts "\nDo you want to try again Y/N"
+    choice = gets.chomp
+    if choice == "Y" || choice == "y"
+      puts "\n-- A new code was generated --
+            \n          -------              "
+      codemaker.create_code
+      game_on = true
+      round_count = 0
+      choice = ""
+    else
+      game_on = false
+      choice = ""
+    end
+  end
 
 end
